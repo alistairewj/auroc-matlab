a87d56a801c649ab9b381ef5b0a46b6ad9b5d594
Alistair Johnson <alistairewj@gmail.com>
2015-07-23 14:48:51 -0400

    Renamed theta as auc

--- wilcoxonCI.m
+++ wilcoxonCI.m
@@ -1,6 +1,6 @@
-function [ theta, thetaCI, S, S10, S01, V10, V01 ] = wilcoxonCI(pred,target, alpha)
+function [ auc, auc_ci, S, S10, S01, V10, V01 ] = wilcoxonCI(pred,target, alpha)
 %WILCOXONCOVARIANCE	Calculates the covariance for R sets of predictions and outcomes
-%	[ theta, thetaCI, S, S10, S01, V10, V01 ] = wilcoxonCI(pred,target, alpha)
+%	[ auc, auc_ci, S, S10, S01, V10, V01 ] = wilcoxonCI(pred,target, alpha)
 %	
 %
 %	Inputs: Must come in pairs in the following order:
@@ -14,11 +14,11 @@ function [ theta, thetaCI, S, S10, S01, V10, V01 ] = wilcoxonCI(pred,target, alp
 %		S01     - Covariance matrix for negative outcomes
 %		V10     - Pairwise comparisons of positive predictions
 %		V01     - Pairwise comparisons of negative predictions
-%       theta   - Row vector containing AUROC/Wilcoxon statistic
+%       auc     - Row vector containing AUROC/Wilcoxon statistic
 %		
 %
 %	Example
-%		[ theta, thetaCI, S, S10, S01, V10, V01 ] = ...
+%		[ auc, auc_ci, S, S10, S01, V10, V01 ] = ...
 %           wilcoxonCI(pred,target, alpha)
 %	
 %	See also WILCOXONEXACT, WILCOXONVARIANCE
@@ -56,11 +56,11 @@ idx=target==1;
 X=pred(idx); m=length(X);
 Y=pred(~idx); n=length(Y);
 
-% % % -------------- THETA, V10, V01 -------------- % % %
-% Using matrices X and Y, calculate estimated Wilcoxon statistic (theta)
+% % % -------------- AUC, V10, V01 -------------- % % %
+% Using matrices X and Y, calculate estimated Wilcoxon statistic (auc)
 % Also Calculate the mxK and nxK V10 and V01 matrices
 
-theta=zeros(1,K);
+auc=zeros(1,K);
 V10=zeros(m,K); 
 V01=zeros(n,K);
 
@@ -70,9 +70,9 @@ for r=1:K % For each X/Y column pair
         phi1=sum(gt(X(i,r),Y(:,r))); % Xi>Y
         phi2=sum(eq(X(i,r),Y(:,r))); % Xi=Y
         V10(i,r)=(phi1+phi2*0.5)/n;
-        theta(r)=theta(r)+phi1+phi2*0.5;
+        auc(r)=auc(r)+phi1+phi2*0.5;
     end
-    theta(r)=theta(r)/(n*m);
+    auc(r)=auc(r)/(n*m);
     for j=1:n
         phi1=gt(X(:,r),Y(j,r)); % X>Yj
         phi2=eq(X(:,r),Y(j,r)); % X=Yj
@@ -81,13 +81,13 @@ for r=1:K % For each X/Y column pair
 end
 
 %  Calculate S01 and S10, covariance matrices of V01 and V10
-S01 = ((V01'*V01)-n*(theta'*theta))/(n-1);
-S10 = ((V10'*V10)-m*(theta'*theta))/(m-1);
-% S01a = ((V01-theta)'*(V01-theta))/(n-1); % Alternative equivalent formulation
-% S10a = ((V10-theta)'*(V10-theta))/(m-1); % Alternative equivalent formulation
+S01 = ((V01'*V01)-n*(auc'*auc))/(n-1);
+S10 = ((V10'*V10)-m*(auc'*auc))/(m-1);
+% S01a = ((V01-auc)'*(V01-auc))/(n-1); % Alternative equivalent formulation
+% S10a = ((V10-auc)'*(V10-auc))/(m-1); % Alternative equivalent formulation
 
 
-% Combine for S, covariance matrix of theta
+% Combine for S, covariance matrix of auc
 S = (1/m)*S10 + (1/n)*S01;
-thetaCI = norminv([alpha/2,1-(alpha/2)],theta,sqrt(S));
+auc_ci = norminv([alpha/2,1-(alpha/2)],auc,sqrt(S));
 end
